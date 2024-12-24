8a42126c20be1b9ee7b06e5d4c18ba46e118099e
yashuvm <48407518+yashuvm@users.noreply.github.com>
2024-12-24 13:17:01 +0530

    Update removeDuplicate.js

--- Array/Easy/removeDuplicate.js
+++ Array/Easy/removeDuplicate.js
@@ -1,18 +1,17 @@
+const result = removeDuplicate([1, 2, 3])
+console.log(result)
+
 function removeDuplicate(ary) {
-  let i = 0;
-  for (let j = 1; j <= ary.length - 1; j++) {
-    if (ary[i] !== ary[j]) {
-      ary[i + 1] = ary[j];
-      ary[j] = "_";
-      i++;
-    }else{
-        ary[j]="_"
+ let lowIndex=0
+ for(let i=1;i<=ary.length-1;i++){
+    if(ary[i]!==ary[lowIndex]){
+    		ary[lowIndex+1]=ary[i]
+        lowIndex++
     }
-  }
-  return ary;
+ }
+ ary.length=lowIndex+1
+ return ary
 }
 
-const result = removeDuplicate([1, 1, 2, 2, 2, 3, 3]);
-console.log(result);
 
 //o(n)
