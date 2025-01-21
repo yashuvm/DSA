6ce54175d0ed5eeb1e2b0d3b280a6ee3e40c9aad
yashuvm <48407518+yashuvm@users.noreply.github.com>
2025-01-21 13:38:17 +0530

    Update missingNumber.js

--- Array/Easy/missingNumber.js
+++ Array/Easy/missingNumber.js
@@ -11,3 +11,19 @@ console.log(result);
 
 //o(n)
 
+const result = missingNumberAry([1, 2, 4, 5], 5);
+console.log(result);
+
+
+function missingNumberAry(ary,num){
+  const obj={}
+ for(let value of ary){
+    obj[value]=true
+  }
+  for(let i=1;i<=num;i++){
+    if(obj[i] ===undefined){
+      console.log("Missing number is " + i)
+    }
+  }
+}
+//////space used and two for loop  time o(n) space o(n) 
