7897938a70924f93f51fc9d290ca5b5affe03b02
yashuvm <48407518+yashuvm@users.noreply.github.com>
2025-02-03 13:53:58 +0530

    Update Lis.js

--- Array/Hard/Lis.js
+++ Array/Hard/Lis.js
@@ -1,3 +1,7 @@
 //LIS -> [10,9,2,5,3,7,101,18] ->[10,101]  [2,5,7,101] -> 4 
 //[0,1,0,3,2,3] ->[0,3] [0,2,3] [0,1,2,3] -> 4 
 //[7,7,7,7] -> 1 
+
+
+//if we find all subsequce then the complexity is increase 2^e exportionaly increase
+//then we are use dynamic programing by storing the result for each step and use in the further or in future 
