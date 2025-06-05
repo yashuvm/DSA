9d956c1f898a551f3de5edbed99fb168f54762c0
yashuvm <48407518+yashuvm@users.noreply.github.com>
2025-06-05 12:53:01 +0000

    pattern

---
+++ Pattern/pattern1.js
@@ -0,0 +1,4 @@
+//1 For the outer loop count the no of rows or lines
+//2 For the inner loop Focus on the column and connect them some how to the rows
+//3 Print the "*" inner for loop 
+//4 Observe symmetry (optional)
