b076ae58b1d31769bc97cfdc275a20b933c7684a
yashuvm <48407518+yashuvm@users.noreply.github.com>
2025-06-09 14:37:32 +0000

    commit new code in missing number

--- Array/Easy/missingNumber.js
+++ Array/Easy/missingNumber.js
@@ -38,8 +38,26 @@ function missingNumberAry(ary,num){
      sum+=ary[i]
    }
    let expressinSum=num*(num+1)/2 
+   //// Calculate the expected sum ->let expSum = (n * (n + 1)) / 2;
    let missingEl=expressinSum-sum
    return missingEl
 }
 //o(n)
 
+function missingNum(arr) {
+    const n = arr.length + 1;
+    let xor1 = 0, xor2 = 0;
+
+    // XOR all array elements
+    for (let i = 0; i < n - 1; i++) {
+        xor2 ^= arr[i];
+    }
+
+    // XOR all numbers from 1 to n
+    for (let i = 1; i <= n; i++) {
+        xor1 ^= i;
+    }
+
+    // Missing number is the XOR of xor1 and xor2
+    return xor1 ^ xor2;
+}
